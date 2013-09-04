547d2c20f6e6fdf8b7823f924b456ce636e38a26
brianp <brian.o.pearce@gmail.com>
2013-09-04 14:13:45 -0700

    Commit updated specs.

--- spec/_plugins/privately_public_spec.rb
+++ spec/_plugins/privately_public_spec.rb
@@ -1,6 +1,6 @@
 require './spec/spec_helper'
 
-describe Jekyll::PrivatelyPublicPost do
+describe Jekyll::PrivatelyPublic do
   before do
     clear_dest
     stub(Jekyll).configuration do
@@ -10,13 +10,44 @@ describe Jekyll::PrivatelyPublicPost do
 
     @site = Site.new(Jekyll.configuration)
     @site.process
+
+    @private_url = '/private/9dbee0/published-post'
   end
 
-  it 'adds 1 post to the privpub_posts collection' do
-    @site.privpub_posts.count.must_equal 1
-  end
-
-  it 'generated 1 file in the private dir' do
+  it 'generates one folder in the private dir' do
     Dir.glob("#{dest_dir('private')}/*").count.must_equal 1
+  end
+
+  it 'generates a consistant url' do
+    folder = Dir.glob("#{dest_dir('private')}/*").first
+    clear_dest
+    @site.process
+    new_folder = Dir.glob("#{dest_dir('private')}/*").first
+    folder.must_equal new_folder
+  end
+
+  describe Jekyll::PrivatelyPublic::Post do
+    subject { Jekyll::PrivatelyPublic::Post }
+
+    it 'creates the same url multiple times' do
+      entry = @site.posts.first.name
+      post = subject.new(@site, source_dir, '', entry)
+
+      post.permalink.must_equal @private_url
+    end
+  end
+
+  describe Jekyll::PrivatelyPublic::Generator do
+    subject { Jekyll::PrivatelyPublic::Generator }
+
+    before do
+      @site = Site.new(Jekyll.configuration)
+    end
+
+    it 'adds one privpub post to the pages array' do
+      gen = subject.new
+      gen.read_posts(@site)
+      @site.pages.count.must_equal 1
+    end
   end
 end
