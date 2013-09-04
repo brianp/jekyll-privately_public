a2455a12b0414bff9308bf4e492bdccb8f4c8426
brianp <brian.o.pearce@gmail.com>
2013-09-04 00:59:36 -0700

    Helpers for managing generated sites.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,25 @@ Bundler.require(:test)
 
 require 'minitest/spec'
 require 'minitest/autorun'
-require 'minitest/mock'
+require 'rr'
 require 'jekyll'
 
 require './_plugins/privately_public'
+
+include Jekyll
+
+def clear_dest
+  FileUtils.rm_rf(dest_dir)
+end
+
+def dest_dir(*subdirs)
+  test_dir('public', *subdirs)
+end
+
+def source_dir(*subdirs)
+  test_dir('source', *subdirs)
+end
+
+def test_dir(*subdirs)
+  File.join(File.dirname(__FILE__), *subdirs)
+end
